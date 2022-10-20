06964d7eb3861c70b6292b1d0a18bf7b564a8345
nonhlalo <nonhlalosibbs@gmail.com>
2022-10-20 02:46:30 -0700

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * prints the alphabet- print alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void);
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+
+	return (0);
+}
