b44aab5e885d49d5f4ced3700771faeadd47ddc6
nonhlalo <nonhlalosibbs@gmail.com>
2022-10-29 09:02:07 -0700

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,9 +2,7 @@
 
 
 /**
- * jack_bauer - function that prints every minute of the day
- * Description: Done
- * Return: Always 0
+ * jack_bauer - prints every minute of the day
  */
 void jack_bauer(void)
 {
@@ -22,5 +20,4 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
-	return (0);
 }
